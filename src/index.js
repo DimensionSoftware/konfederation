cd0647b72bcc4665b34fce815d3039f7a9de6044
John BEPPU <john.beppu@gmail.com>
2017-02-09 01:57:10 -0800

    Fix comments for jsdoc

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-require("babel-core/register");
-require("babel-polyfill");
+// require("babel-core/register");
+// require("babel-polyfill");
 
-import koa from 'koa';
+import koa      from 'koa';
 import passport from 'passport';
-import Router from 'koa-router';
+import Router   from 'koa-router';
 
 var routes = [
   '/login',           // local
@@ -13,52 +13,52 @@ var routes = [
 ];
 
 /**
-   Login POST request handler for local strategy
-
-   @param {Function} next
-   @return {Void}
-*/
+ * Login POST request handler for local strategy
+ *
+ * @param {Function} next
+ * @return {Void}
+ */
 function *login(next) {
   next();
 }
 
 /**
-   Logout GET request for all strategies
-
-   @param {Function} next
-   @return {Void}
-*/
+ * Logout GET request for all strategies
+ *
+ * @param {Function} next
+ * @return {Void}
+ */
 function *logout(next) {
   next();
 }
 
 /**
-   Forgot Password POST request for local strategy
-
-   @param {Function} next
-   @return {Void}
-*/
+ * Forgot Password POST request for local strategy
+ *
+ * @param {Function} next
+ * @return {Void}
+ */
 function *forgotPassword(next) {
   next();
 }
 
 /**
-   Register POST request for local strategy
-
-   @param {Function} next
-   @return {Void}
-*/
+ * Register POST request for local strategy
+ *
+ * @param {Function} next
+ * @return {Void}
+ */
 function *register(next) {
   next();
 }
 
 /**
-  Return a koa app with authentication routes for the given userStorage and passport strategies.
-
-  @param {UserStorage} userStorage
-  @param {Array<PassportStrategy>} passports
-  @return {Koa}
-*/
+ * Return a koa app with authentication routes for the given userStorage and passport strategies.
+ *
+ * @param {UserStorage} userStorage
+ * @param {Array<PassportStrategy>} passports
+ * @return {Koa}
+ */
 function konfederation(userStorage, passports) {
   let app    = koa();
   let router = new Router();
