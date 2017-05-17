a374d20ac708540f83b32f4ef4bd22cc5d0ad93c
John Beppu <john.beppu@gmail.com>
2017-05-17 16:51:12 -0700

    Move async functions inside konfederation function
    Those functions needed to be in the scope of the variables
    that the konfederation function receives.

--- src/app.js
+++ src/app.js
@@ -1,56 +1,9 @@
-// require("babel-core/register");
-// require("babel-polyfill");
-
-import koa      from 'koa';
-import passport from 'passport';
-import Router   from 'koa-router';
-
-var routes = [
-  '/login',           // local
-  '/logout',          // all
-  '/forgot-password', // local
-  '/register'         // local
-];
-
-/**
- * Login POST request handler for local strategy
- *
- * @param {Function} next
- * @return {Void}
- */
-function *login(next) {
-  next();
-}
-
-/**
- * Logout GET request for all strategies
- *
- * @param {Function} next
- * @return {Void}
- */
-function *logout(next) {
-  next();
-}
-
-/**
- * Forgot Password POST request for local strategy
- *
- * @param {Function} next
- * @return {Void}
- */
-function *forgotPassword(next) {
-  next();
-}
-
-/**
- * Register POST request for local strategy
- *
- * @param {Function} next
- * @return {Void}
- */
-function *register(next) {
-  next();
-}
+import Koa        from 'koa';
+import Router     from 'koa-router';
+import convert    from 'koa-convert';
+import bodyParser from 'koa-bodyparser';
+import passport   from 'koa-passport';
+import session    from 'koa-session';
 
 /**
  * Return a koa app with authentication routes for the given userStorage and passport strategies.
@@ -59,15 +12,78 @@ function *register(next) {
  * @param {Array<PassportStrategy>} passports
  * @return {Koa}
  */
-function konfederation(userStorage, passports) {
-  let app    = koa();
-  let router = new Router();
-  app.use(function *(){
-    router.post(login);
-    router.get(logout);
-    router.post(forgotPassword);
-    router.post(register);
+function konfederation(userStorage, passports=[], sess) {
+  var app    = new Koa();
+  var router = new Router();
+
+  /**
+   * Login POST request handler for local strategy
+   *
+   * @param {Function} next
+   * @return {Promise}
+   */
+  async function login(ctx, next) {
+    next();
+  }
+
+  /**
+   * Logout GET request for all strategies
+   *
+   * @param {Function} next
+   * @return {Promise}
+   */
+  async function logout(ctx, next) {
+    next();
+  }
+
+  /**
+   * Forgot Password POST request for local strategy
+   *
+   * @param {Function} next
+   * @return {Promise}
+   */
+  async function forgotPassword(ctx, next) {
+    next();
+  }
+
+  /**
+   * Register POST request for local strategy
+   *
+   * @param {Function} next
+   * @return {Promise}
+   */
+  async function register(ctx, next) {
+    next();
+  }
+
+  var routes = [
+    ['post', '/login'],            // local
+    ['get',  '/logout'],           // all
+    ['post', '/forgot-password'],  // local
+    ['post', '/register']          // local
+  ];
+
+  routes.forEach((r, i) => {
+    let method = r[0];
+    let fn     = r[1];
+    router[method](fn);
   });
+
+  if (!sess) {
+    sess = session({}, app);
+  }
+
+  passports.forEach((p, i) => {
+
+  });
+
+  app
+    .use(bodyParser)
+    .use(session)
+    .use(passport.initialize())
+    .use(passport.session())
+    .use(convert(router.routes()))
+    .use(convert(router.allowedMethods()));
   return app;
 }
 
